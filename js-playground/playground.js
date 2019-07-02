c8a2d6486b695d7a8932a64db203b71069f707a2
furkan-ozbudak <ozbudakfurkan@gmail.com>
2019-07-02 16:52:45 -0500

    Text decorator and ASCII animation is coded. There are few bugs to keep working on.

---
+++ js-playground/playground.js
@@ -0,0 +1,7 @@
+const s = "Connie Client";
+let fName = s.substring(0, s.indexOf(" "));
+let len = s.length; // 13
+let s2 = 'Melvin Merchant'; // can use "" or ' '
+console.log(fName);
+console.log(len);
+console.log(s2);
